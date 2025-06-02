fe2ba5eecabb86e177d5a2aed86e01cdafe0f073
logoes0 <logpanja2k4@gmail.com>
2025-06-03 01:47:27 +0530

    base structure

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,8 @@
 module github.com/logoes0/peeriodic.git
 
 go 1.24.3
+
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/joho/godotenv v1.5.1
+)
--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,37 @@
-package backend
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := chi.NewRouter()
+
+	r.Get("/start", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		json.NewEncoder(w).Encode(map[string]string{
+			"status":  "success",
+			"message": "initial run successful",
+		})
+	})
+
+	local_url := "http://localhost:" + port
+	fmt.Println("🚀 Server running on:", local_url)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
